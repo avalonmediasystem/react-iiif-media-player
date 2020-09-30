4400c2d5cd6b45bb84e862c4b5c0d17cc8cc4837
Dananji <dan9131@gmail.com>
2020-09-30 08:53:35 -0400

    Updates

--- 135.bb2c47c5510f5e0b6971.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[135],{300:function(n,t,o){"use strict";o.r(t),o.d(t,"startStatusTap",(function(){return i}));var e=o(32),i=function(){var n=window;n.addEventListener("statusTap",(function(){Object(e.g)((function(){var t=n.innerWidth,o=n.innerHeight,i=document.elementFromPoint(t/2,o/2);if(i){var c=i.closest("ion-content");c&&c.componentOnReady().then((function(){Object(e.n)((function(){return c.scrollToTop(300)}))}))}}))}))}}}]);
-//# sourceMappingURL=135.bb2c47c5510f5e0b6971.js.map
